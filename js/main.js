54e8f1f4852eb14b966b42b694e10e534390e508
Delius <pawel@fibrotrust.org>
2014-10-09 17:22:30 +0100

    add models view and tasks

--- js/main.js
+++ js/main.js
@@ -11,87 +11,42 @@
 	window.template = function(id) {
 		return _.template( $('#' + id).html() );
 	};
-
-
-
-//Person Model
-
-App.Models.Person = Backbone.Model.extend({
-	defaults: {
-		name: 'John',
-		age: 30,
-		occupation: 'worker'
-	}
-});
-
-//A list of People
-
-App.Collections.People = Backbone.Collection.extend({
-	model: App.Models.Person
-});
-
-//View for all People
-
-App.Views.People = Backbone.View.extend({
-	tagName: 'ul',
-
-	render:function() {
-		this.collection.each(function(person) {
-			var personView = new App.Views.Person({ model: person});
-			this.$el.append(personView.render().el);
-		}, this);
-
-		return this;
-
-	}
-
-
-});
-
-//View for Person
-
-App.Views.Person = Backbone.View.extend({
-	tagName: 'li',
-
-	template: template('personTemplate'),
 	
-	initialize: function() {
-		this.render();
+
+	App.Models.Task = Backbone.Model.extend({});
+	App.Collections.Tasks = Backbone.Collection.extend({
+		model: Tasks
+	});
+
+
+	App.Views.Task = Backbone.View.extend({
+		tagName: 'li',
+
+		render: function() {
+			this.$el.html(this.model.get('title'));
+			return this;
+		}
+	});
+
+
+	var tasks = new App.Collections.Tasks([
+	{
+		title: 'Go to the Store',
+		priority: 4
 	},
-
-	render: function() {
-		this.$el.html( this.template(this.model.toJSON()));
-		return this;
+	{
+		title: 'Go to the mall',
+		priority: 2
+	},
+	{
+		title: 'Go to work',
+		priority: 5
 	}
-});
 
-
-
-var peopleCollection = new App.Collections.People([
-
-{
-	name: 'Jeff',
-	age: 21
-
-},
-
-{
-	name: 'John',
-	age: 23
-
-},
-
-{
-	name: 'Jane',
-	age: 23
-
-}
 	]);
 
 
 
 
-var peopleView = new App.Views.People( { collection: peopleCollection } );
-$(document.body).append(peopleView.render().el);
 
 })();
